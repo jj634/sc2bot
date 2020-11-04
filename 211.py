7a6e73626a7ac03522f5eb9f246aabef409f91c4
Jason Jung <jj634@cornell.edu>
2020-11-04 13:57:41 -0500

    building depots

--- 211.py
+++ 211.py
@@ -13,15 +13,24 @@ class TOOBot(sc2.BotAI):
         # If we don't have a townhall anymore, send all units to attack
         ccs: Units = self.townhalls(UnitTypeId.COMMANDCENTER)
         if not ccs:
+            print("no ccs!")
             target: Point2 = self.enemy_structures.random_or(self.enemy_start_locations[0]).position
             for unit in self.workers | self.units(UnitTypeId.MARINE):
                 unit.attack(target)
             return
         else:
             cc: Unit = ccs.first
+
         # Train more SCVs
-        if self.can_afford(UnitTypeId.SCV) and self.supply_workers < 15 and cc.is_idle:
+        if self.can_afford(UnitTypeId.SCV) and self.supply_workers < 60 and cc.is_idle:
+            print("training scv!")
             cc.train(UnitTypeId.SCV)
+
+        # Build supply depots
+        if self.supply_left < 4 and self.supply_used >= 14:
+            if self.can_afford(UnitTypeId.SUPPLYDEPOT) and self.already_pending(UnitTypeId.SUPPLYDEPOT) < 2:
+                print("building depot!")
+                await self.build(UnitTypeId.SUPPLYDEPOT, near=cc.position.towards(self.game_info.map_center, 5))
 
 
 def main():
