dbd7acdc9da4844ae09e3646f0ad968094d2cf9c
Jason Jung <jj634@cornell.edu>
2020-12-23 18:23:53 -0500

    no longer stuck on orbital, added train_workers_until

--- two-one-one/bot/bot.py
+++ two-one-one/bot/bot.py
@@ -18,9 +18,6 @@ class TOOBot(sc2.BotAI):
         # Do things here before the game starts
 
     async def on_step(self, iteration):
-        # await self.distribute_workers()
-        
-
         # If we don't have a townhall anymore, send all units to attack
         ccs: Units = self.townhalls
         if ccs is None:
@@ -31,6 +28,12 @@ class TOOBot(sc2.BotAI):
             return
         else:
             cc: Unit = ccs.first
+
+
+        # Build order begins
+
+        await self.train_workers_until(19)
+        # TODO: make sure that the build doesn't continue in the middle later on in the game
 
         # 14 depot
         if self.supply_used == 14 and self.can_afford(UnitTypeId.SUPPLYDEPOT) and self.already_pending(UnitTypeId.SUPPLYDEPOT) == 0:
@@ -59,20 +62,23 @@ class TOOBot(sc2.BotAI):
                 # Only issue one build geysir command per frame
                 break
 
+        # 19 orbital command
+        if self.supply_used == 19 and self.can_afford(UnitTypeId.ORBITALCOMMAND):
+            if self.structures(UnitTypeId.BARRACKS).ready:
+                cc.build(UnitTypeId.ORBITALCOMMAND)
+
         # 19 reaper
         if self.supply_used == 19 and self.can_afford(UnitTypeId.REAPER):
             barrack = self.structures(UnitTypeId.BARRACKS).idle.random_or(None)
             if barrack:
                 barrack.train(UnitTypeId.REAPER)
 
-        # 19 orbital command
-        if self.structures(UnitTypeId.BARRACKS).ready and self.can_afford(UnitTypeId.ORBITALCOMMAND):
-            
-            cc.build(UnitTypeId.ORBITALCOMMAND)
-
         # 20 expand
         if self.supply_used == 20 and self.can_afford(UnitTypeId.COMMANDCENTER):
             await self.build(UnitTypeId.COMMANDCENTER, near=cc.position.towards(self.game_info.map_center, 5))
+
+        if (self.supply_used >= 20):
+            await self.train_workers()
 
         # 20 second barracks
 
@@ -105,25 +111,44 @@ class TOOBot(sc2.BotAI):
         # 53 depot
         
 
+        # TODO: improve this, see why distribute_workers doesn't work
+        # await self.distribute_workers()
         for a in self.gas_buildings:
             if a.assigned_harvesters < a.ideal_harvesters:
                 w = self.workers.closer_than(20, a)
                 if w:
                     w.random.gather(a)
-
         for scv in self.workers.idle:
             scv.gather(self.mineral_field.closest_to(cc))
 
-        await self.train_workers()
+
         
 
     async def train_workers(self):
+        """
+        Continuously trains workers until every base has 22 workers.
+        """
         # a random idle cc, or None if no idle cc's
         cc = self.townhalls.idle.random_or(None)
         if (
             cc and
             self.can_afford(UnitTypeId.SCV) and
             self.supply_workers < self.townhalls.amount * 22
+        ):
+            cc.train(UnitTypeId.SCV)
+
+
+    async def train_workers_until(self, until_supply: int):
+        """
+        Continuously trains workers until the current supply is until_supply.
+        :param until_supply:
+        """
+        # a random idle cc, or None if no idle cc's
+        cc = self.townhalls.idle.random_or(None)
+        if (
+            cc and
+            self.can_afford(UnitTypeId.SCV) and
+            self.supply_used < until_supply
         ):
             cc.train(UnitTypeId.SCV)
 
